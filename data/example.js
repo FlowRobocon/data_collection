921da0772e2cb189fee1b149c62541695f92c546
FlowRobocon <109451450+FlowRobocon@users.noreply.github.com>
2022-07-17 14:38:48 +0900

    Add files via upload

---
+++ data/example.js
@@ -0,0 +1,24 @@
+example = [
+    {
+        id: 1,
+        speaker: "user",
+        intent: "教える",
+        slots: [
+            {
+                user_name: "鈴木"
+            }
+        ]
+    },
+	{
+		id: 2,
+		speaker: "system",
+		intent: "観光地の名称を確認する",
+		slots: [
+            {
+                name: "鈴木",
+                sightname_1: "お台場海浜公園",
+                sightname_1: "第三台場"
+            }
+        ]
+	}
+];
